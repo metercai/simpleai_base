d084687586ca3ba0240b2d1294b6b13ecc153b07
metercai <cai.mingjun@gmail.com>
2024-05-03 00:20:36 +0800

    fixed

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use base64::Engine;
 
 use pyo3::prelude::*;
 use crate::token::TokenDid;
-use crate::claim::IdClaim;
+use crate::claim::{IdClaim, SystemInfo};
 
 
 mod claim;
@@ -47,5 +47,6 @@ fn tokendid(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(get_current_dir, m)?)?;
     m.add_class::<TokenDid>()?;
     m.add_class::<IdClaim>()?;
+    m.add_class::<SystemInfo>()?;
     Ok(())
 }
