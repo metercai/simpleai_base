eebc29fb86f34c0df69f6c91f5edd32a5b7024d9
metercai <cai.mingjun@gmail.com>
2024-05-10 11:49:10 +0800

    torch_xformers_cuda_version

--- python/simpleai/__init__.py
+++ python/simpleai/__init__.py
@@ -1,12 +1,34 @@
-import asyncio
+import os
+import importlib.util
 
-from simpleai import simpleai
-
-
-async def async_task():
-    global public_ip, port
-    public_ip = await simpleai.get_ipaddr_from_public(False)
-    port = await simpleai.get_port_availability(simpleai.get_ipaddr_from_stream(), 8186)
+#from simpleai import simpleai
 
 
-#asyncio.run(async_task())
+def get_torch_xformers_cuda_version():
+    try:
+        torch_version = ""
+        cuda_version = ""
+        torch_spec = importlib.util.find_spec("torch")
+        for folder in torch_spec.submodule_search_locations:
+            ver_file = os.path.join(folder, "version.py")
+            if os.path.isfile(ver_file):
+                spec = importlib.util.spec_from_file_location("torch_version_import", ver_file)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+                torch_version = module.__version__
+                cuda_version = getattr(module, 'cuda', "")
+        xformers_version = ""
+        xformers_spec = importlib.util.find_spec("xformers")
+        for folder in xformers_spec.submodule_search_locations:
+            ver_file = os.path.join(folder, "version.py")
+            if os.path.isfile(ver_file):
+                spec = importlib.util.spec_from_file_location("xformers_version_import", ver_file)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+                xformers_version = module.__version__
+    except:
+        pass
+    return torch_version, xformers_version, cuda_version
+
+torch_version, xformers_version, cuda_version = get_torch_xformers_cuda_version()
+
