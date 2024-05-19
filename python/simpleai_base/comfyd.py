5484309bd30780f4608913b733ceb5d2631c5349
metercai <cai.mingjun@gmail.com>
2024-05-19 20:00:42 +0800

    update to 0.3.2

--- python/simpleai_base/comfyd.py
+++ python/simpleai_base/comfyd.py
@@ -3,5 +3,5 @@ import os
 import sys
 
 backend_script = os.path.join(os.getcwd(),'comfy/main.py')
-arguments = ["--preview-method", "auto", "--listen", "--port", "8188", "--disable-auto-launch"]
+arguments = ["--preview-method", "auto", "--port", "8188", "--disable-auto-launch"]
 comfyd_process  = subprocess.Popen([sys.executable, backend_script] + arguments)
