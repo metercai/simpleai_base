8827c12cd25e2429a8f7be7aa544d6eb00a83d8e
metercai <cai.mingjun@gmail.com>
2024-06-26 13:58:36 +0800

    update to 0.3.12

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='simpleai_base',
+    version='0.3.0',
+    packages=['simpleai_base'],
+    install_requires=[
+        'maturin @ git+https://github.com/PyO3/maturin.git@v0.14.0',
+    ],
+    include_package_data=True,
+    description='A Python package with Rust code',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/my_python_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
