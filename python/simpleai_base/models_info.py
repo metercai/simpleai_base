35467a4b6e5fbe6ff110d1310eaf0ad30ec999a0
metercai <cai.mingjun@gmail.com>
2024-09-10 00:31:25 +0800

    update to 0.3.18

--- python/simpleai_base/models_info.py
+++ python/simpleai_base/models_info.py
@@ -488,7 +488,7 @@ class ModelsInfo:
         new_file_key = []
         del_file_key = []
 
-        print(f'm_info_key:{self.m_info.keys()}')
+        #print(f'm_info_key:{self.m_info.keys()}')
         for path in self.path_map.keys():
             if self.path_map[path]:
                 path_filenames = self.get_path_filenames(path)
@@ -509,7 +509,8 @@ class ModelsInfo:
                         new_info_key.append(model_key)
                     if model_key not in self.m_info.keys():
                         new_model_key.append(model_key)
-        #print(f'[ModelInfo] new_model_key:{new_model_key}, new_file_key:{new_file_key}')
+        print(f'[ModelInfo] new_model_key:{new_model_key}, new_file_key:{new_file_key}')
+        print(f'[ModelInfo] del_model_key:{del_model_key}, del_file_key:{del_file_key}')
         for k in self.m_info.keys():
             if k not in new_info_key:
                 del_model_key.append(k)
@@ -683,6 +684,7 @@ class ModelsInfo:
         return False
 
     def exists_model_key(self, model_key):
+        print(f'exists_model_key: {model_key}, {self.m_info.keys()}')
         if model_key in self.m_info:
             return True
         return False
