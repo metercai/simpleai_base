e95bd5a55f93515d5ff1af6d7b02dc46e5e2201f
metercai <cai.mingjun@gmail.com>
2024-10-24 01:16:04 +0800

    update to 0.3.21

--- src/token_utils.rs
+++ src/token_utils.rs
@@ -39,7 +39,7 @@ const ALGORITHM_ID: pkcs8::AlgorithmIdentifierRef<'static> = pkcs8::AlgorithmIde
 };
 
 pub(crate) static TOKEN_TM_URL: &str = "https://v2.token.tm/api_";
-pub(crate) static TOKEN_TM_DID: &str = "BT4jD4M5uQRxrwsaXQVtFxDPf28RC";
+pub(crate) static TOKEN_TM_DID: &str = "2nMGHiVunD2QaTnJYHD5aQLFnHnBh";
 
 lazy_static! {
     pub static ref SYSTEM_BASE_INFO: SystemBaseInfo = SystemBaseInfo::generate();
